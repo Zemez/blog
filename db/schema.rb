9927bbd71fee2ead3c2a20f18e90fd012e46acfa
Andrey Inozemtsev <zemez@inbox.ru>
2018-09-27 17:54:37 +0300

    Скрытый символ затесался

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2018_09_27_121028) do
     t.string "title"
     t.text "body"
     t.bigint "user_id"
-    t.boolean "visible​", default: false
+    t.boolean "visible", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["title"], name: "index_posts_on_title", unique: true
