0c8a4616f3ad71a28ccf9d97d7f542975e83f0dc
Andrey Inozemtsev <zemez@inbox.ru>
2018-09-27 17:55:16 +0300

    Задание 3.1 выполнено

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
+  validates :title, uniqueness: true
+  validates :title, length: { minimum: 8, maximum: 255 }
+
+  validates :body, presence: true
+  validates :body, length: { minimum: 1, maximum: 8192 }
 end
