2258035f32afa8fc89b22903f834ef29ce19783b
Andrey Inozemtsev <zemez@inbox.ru>
2018-10-11 14:16:42 +0300

    Задание 7.1 выполнено.

---
+++ app/javascript/javascripts/test-webpacker.js
@@ -0,0 +1,2 @@
+/* test-webpacker.js */
+console.log('Hello World from Webpacker')
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,3 @@
-/* eslint no-console:0 */
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -7,4 +6,15 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+/* application,js */
+import 'stylesheets/application';
+
+import Rails from 'rails-ujs';
+import Turbolinks from 'turbolinks';
+import 'bootstrap';
+
+Rails.start();
+Turbolinks.start();
+
+import 'javascripts/xray';
+import 'javascripts/test-webpacker';
